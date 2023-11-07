fca366603e1c0a8b62e6a006b6d2584f86004aa0
שפרה מאיר <214493322@mbydomain>
2023-11-07 18:02:59 +0200

    achange to ID

--- Products/Product.cs
+++ Products/Product.cs
@@ -2,7 +2,7 @@
 {
     public class Product
     {
-        public string Id { get; set; }
+        public string ID { get; set; }
         public string Name { get; set; }
         public int Price { get; set; }
     }
